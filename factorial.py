7ce63df21ecefcdf084834fa2c0d6a5233baf526
Valerie Walsh <35744819+vwalsh86@users.noreply.github.com>
2018-03-13 08:20:07 +0000

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,30 @@
+# Valerie Walsh 13-03-2018
+# Topic 6: Functions - Exercise Factorial
+# Resource: https://stackoverflow.com/questions/5136447/function-for-factorial-in-python
+
+def factorial(i):
+    num = 1
+    while i >= 1:
+        num = num * i
+        i = i - 1
+    return num
+
+
+print("The factorial of 5 is", factorial(5))
+print("The factorial of 7 is", factorial(7))
+print("The factorial of 10 is", factorial(10), "\n")
+
+
+# I thought the above could be condensed further into a shorter fuction and continued to research, I discovered this method
+# Resource: https://www.quora.com/How-would-you-define-a-factorial-function-in-Python
+
+def factorial(f):
+    if f == 0:
+        return 1
+    else:
+        return f * factorial(f-1)
+
+        
+print("The factorial of 5 is", factorial(5))
+print("The factorial of 7 is", factorial(7))
+print("The factorial of 10 is", factorial(10))
