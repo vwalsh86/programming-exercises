d55e83dc8027cc7ceb93ac336d4af165dcc18b59
Valerie Walsh <35744819+vwalsh86@users.noreply.github.com>
2018-04-08 15:11:08 +0100

    Update and rename Topic 5: Iris Data Set.py to Iris Data Set.py

--- Iris Data Set.py
+++ Iris Data Set.py
@@ -1,6 +1,6 @@
 # Valerie Walsh
-# Topic 5 Input and Output 07/03/2018
-# Exercise Irish Flower Data Set
+# Topic 5: Input and output 
+# Write a Python script that reads the Iris data set in and prints the four numerical values on each row in a nice format.
 # The idea to use {:^6} came from reviewing = https://pyformat.info/ - Padding and aligning strings
 # The order of the values is as per the wording of the Exercise requirements - Petal length, Petal Width, Sepal Length, Sepal Width
 # Made multiple attempts to name the columns but failed, left in the coding to show my attempt - https://stackoverflow.com/questions/11346283/renaming-columns-in-pandas
