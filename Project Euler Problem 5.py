3d95366aba664983cb83fb08eee3f5be07e2c417
Valerie Walsh <35744819+vwalsh86@users.noreply.github.com>
2018-04-08 15:19:42 +0100

    Update and rename Project Euler 5.py to Project Euler Problem 5.py

--- Project Euler Problem 5.py
+++ Project Euler Problem 5.py
@@ -1,10 +1,11 @@
 # Valerie Walsh 2018.02.25
-# Reference: Euler Problem 5 https://projecteuler.net/problem=5
+
 # Topic 4: Lists 
 # Write a Python program using for and range to calculate the smallest positive number that is evenly divisible by all of the numbers from 1 to 20.
 # I found this extremely difficult and got to the stage where I had the below layout apart from the correct formulas and also the 'break'
 # Referenced student work for clarification, found online solutions too complex - https://github.com/Sarahkel/Python-Problem-Sets/blob/master/Euler5.py
 
+# Reference: Euler Problem 5 https://projecteuler.net/problem=5
 
 i = 2520
 n = 1
