160ea20cc079cf0d938005eb650d557a5d85f701
Valerie Walsh <35744819+vwalsh86@users.noreply.github.com>
2018-04-08 19:51:05 +0100

    Update Iris Data Set.py
    Added additional notes showing the work I have tried to run to get headings to add to the print out of the script but have so far failed.

--- Iris Data Set.py
+++ Iris Data Set.py
@@ -9,3 +9,24 @@ with open("data/iris.csv") as f:
     f.colums = ['Petal Length', 'Petal Width', 'Sepal Length', 'Sepal Width']
     for line in f:
         print('{:^6} {:^6} {:^6} {:^6}'.format(line.split(',')[2], line.split(',')[3], line.split(',')[0], line.split(',')[1]))
+
+# Made attempts to try add column names / headers but unable to resolve. I have added comments below of the additional code I have tried in order to resolve this.
+# import sqlite3
+# connection = sqlite3.connect('~/foo.sqlite')
+# cursor = connection.execute('select * from bar')
+#
+# import pandas as pd
+# import numpy as np
+# 
+# df = pd.DataFrame(f,columns=['Petal Length', 'Petal Width', 'Sepal Length', 'Sepal Width'])
+#
+# PRAGMA table_info(tablename);
+#
+# df = f.DataFrame(
+#   data = [
+#   ('Petal Length'),
+#   ('Petal Width'),
+#   ('Sepal Length')
+#   ('Sepal Width')
+#   ] 
+# )
